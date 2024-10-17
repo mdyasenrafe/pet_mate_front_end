c13eeed7055345918454331f17525dddd3800dad
Md yasen rafe <mdyasenrafe@gmail.com>
2024-10-18 04:33:40 +0600

    added right side bar

--- src/components/layouts/CommonLayout/componens/RightSideBar/RightSideBar.tsx
+++ src/components/layouts/CommonLayout/componens/RightSideBar/RightSideBar.tsx
@@ -1,5 +1,36 @@
+"use client";
+
+import { Button, Text } from "@/components/atoms";
 import React from "react";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { useRouter } from "next/navigation";
 
 export const RightSideBar = () => {
-  return <div>RightSideBar</div>;
+  const router = useRouter();
+
+  const handleLoginRedirect = () => {
+    router.push("/signin");
+  };
+
+  return (
+    <div className="ml-3 bg-secondary p-3 mt-4 rounded-md">
+      <Text className="text-white font-bold mb-2" variant="p3">
+        Join the Pet Care Community
+      </Text>
+      <Text variant="p5" color="white">
+        Discover expert tips, heartwarming pet stories
+      </Text>
+      <Button
+        className="mt-3 w-full !h-[40px] !border !border-white bg-primary-hover"
+        customColor="secondary"
+        icon={<FaArrowRightLong />}
+        iconPosition="end"
+        onClick={handleLoginRedirect}
+      >
+        <Text className="text-white" variant="p4">
+          Get Started
+        </Text>
+      </Button>
+    </div>
+  );
 };
