e4f4a75097df5c381ce6f087f7d1c34199acccbd
Md yasen rafe <mdyasenrafe@gmail.com>
2024-10-19 02:32:16 +0600

    updated base api url

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../redux/store";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://petmateserver.vercel.app/",
+  baseUrl: "https://petmateserver.vercel.app/api/",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
     if (token) {
