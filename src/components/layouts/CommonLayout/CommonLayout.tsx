ec09d31789b6e2662d448b70d1f17e645116790d
Md yasen rafe <mdyasenrafe@gmail.com>
2024-10-19 23:23:31 +0600

    deploy issue fixed

--- src/components/layouts/CommonLayout/CommonLayout.tsx
+++ src/components/layouts/CommonLayout/CommonLayout.tsx
@@ -1,29 +1,31 @@
 import { Container } from "@/components/atoms";
 import { Row, Col } from "antd";
 import { TProps } from "@/types";
-import React from "react";
+import React, { Suspense } from "react";
 import { BottomNavBar } from "../BottomNavbar";
 import { LeftSideBar, RightSideBar } from "./components";
 
 export const CommonLayout: React.FC<TProps> = ({ children }) => {
   return (
-    <Container>
-      <Row>
-        <Col xs={0} lg={4}>
-          <LeftSideBar />
-        </Col>
-        <Col xs={24} lg={16} className="border-r">
-          <main>{children}</main>
-        </Col>
+    <Suspense>
+      <Container>
+        <Row>
+          <Col xs={0} lg={4}>
+            <LeftSideBar />
+          </Col>
+          <Col xs={24} lg={16} className="border-r">
+            <main>{children}</main>
+          </Col>
 
-        <Col xs={0} lg={4}>
-          <RightSideBar />
-        </Col>
+          <Col xs={0} lg={4}>
+            <RightSideBar />
+          </Col>
 
-        <Col xs={24} lg={0}>
-          <BottomNavBar />
-        </Col>
-      </Row>
-    </Container>
+          <Col xs={24} lg={0}>
+            <BottomNavBar />
+          </Col>
+        </Row>
+      </Container>
+    </Suspense>
   );
 };
