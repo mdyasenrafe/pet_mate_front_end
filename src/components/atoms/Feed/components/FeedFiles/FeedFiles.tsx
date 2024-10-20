0801a3b8fe11cc71c6629e7a797d2d5421927af1
Md yasen rafe <mdyasenrafe@gmail.com>
2024-10-21 00:03:04 +0600

    updated feed fiels

--- src/components/atoms/Feed/components/FeedFiles/FeedFiles.tsx
+++ src/components/atoms/Feed/components/FeedFiles/FeedFiles.tsx
@@ -8,33 +8,41 @@ type FeedFilesProps = {
 export const FeedFiles: React.FC<FeedFilesProps> = ({ files }) => (
   <>
     {files && files.length > 0 && (
-      <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 gap-2">
-        {files.map((file, index) => {
-          if (file.type === "image") {
-            return (
-              <img
-                key={index}
-                src={file.url}
-                alt={`File-${index}`}
-                className="object-cover rounded-md"
-              />
-            );
-          }
-          if (file.type === "pdf") {
-            return (
-              <a
-                key={index}
-                href={file.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center justify-center border border-gray-300 p-4 rounded-md"
-              >
-                <span className="text-gray-700">View PDF</span>
-              </a>
-            );
-          }
-        })}
-      </div>
+      <>
+        <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 gap-2">
+          {files.map((file, index) => {
+            if (file.type === "image") {
+              return (
+                <img
+                  key={index}
+                  src={file.url}
+                  alt={`File-${index}`}
+                  className="object-cover rounded-md"
+                />
+              );
+            }
+            return null;
+          })}
+        </div>
+        <div className="mt-4 ">
+          {files.map((file, index) => {
+            if (file.type === "pdf") {
+              return (
+                <a
+                  key={index}
+                  href={file.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex items-center justify-center border border-gray-300 p-4 rounded-md"
+                >
+                  <span className="text-gray-700">View PDF</span>
+                </a>
+              );
+            }
+            return null;
+          })}
+        </div>
+      </>
     )}
   </>
 );
